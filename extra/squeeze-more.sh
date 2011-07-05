291c0b63ba602251de833211c5f0ac3d88e95f38
David Brown <david@medicmobile.org>
2011-07-04 21:29:10 -0700

    Minified version, known-good -- minified with uglifyjs plus a hand-coded symbol replacement script.

---
+++ extra/squeeze-more.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec sed 's/impl/z/g; s/priv/y/g; s/instance_data_for/z/g; s/toggle/y/g; s/calculate_arrow_delta/x/g; s/_serial_number/w/g; s/_zindex_base/v/g; s/event_to_point/u/g; s/listify/t/g; s/index_of_max/s/g; s/ *\/>/\/>/g; s/ratio/r/g;'
